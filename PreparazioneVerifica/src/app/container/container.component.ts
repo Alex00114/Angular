07c4ddcf07c12bfd762ce20c47e3875d5fa64351
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-10-06 09:26:41 +0000

    punto 2

--- PreparazioneVerifica/src/app/container/container.component.ts
+++ PreparazioneVerifica/src/app/container/container.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-
-  constructor() { }
-
+  macchine: string[];
+  constructor() { 
+     this.macchine = ["Gru", "Ruspa", "Escavatore", "Dumpers", "Finitrici", "Bulldozer", "Trencher", "Autocarro", "Betoniere", "Terne"];
+  }
   ngOnInit(): void {
   }
 
