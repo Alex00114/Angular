73538fa13ebb7d328f63911bc4545b4a69267c25
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2023-05-08 08:51:43 +0000

    02.2 finito

--- maps-geojson/src/app/app.component.ts
+++ maps-geojson/src/app/app.component.ts
@@ -28,5 +28,37 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('mapRef') mapRef!: GoogleMap;
   ngAfterViewInit() {
     this.mapRef.data.addGeoJson(this.geoJsonObject);
-}
+    this.mapRef.data.setStyle(this.styleFunc)
+  }
+  styleFunc = (feature:any) =>{
+    console.log(feature.h.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.h.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+      clickable: false,
+      fillColor: newColor,
+      strokeWeight: 1
+    });
+  }
+
+  markerGenerator() {
+    this.markerList =[
+      {
+        position : {
+          lng : this.geoJsonObject.features[0].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[0].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[0].properties.id),
+      },
+      {
+        position : {
+          lng : this.geoJsonObject.features[1].geometry.coordinates[0][0][0],
+          lat : this.geoJsonObject.features[1].geometry.coordinates[0][0][1]
+        },
+        label: String(this.geoJsonObject.features[1].properties.id),
+      },
+    ]
+
+  }
 }
