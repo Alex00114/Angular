b65377cbb75027922ab4d8ca18da5f2e11078799
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2023-10-25 10:58:17 +0000

    lista iniziale

--- Pokemon/src/app/app.component.ts
+++ Pokemon/src/app/app.component.ts
@@ -10,18 +10,18 @@ import { Risultati } from './models/risultati.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  obsPokemon!: Observable<any>;
-  data!: any;
+  obsPokemon!: Observable<Risultati[]>;
+  data!: Risultati[];
 
   constructor(public http:HttpClient){ }
 
 
   ngOnInit(): void {
-    this.obsPokemon = this.http.get<any>('https://pokeapi.co/api/v2/type')
+    this.obsPokemon = this.http.get<Risultati[]>('https://pokeapi.co/api/v2/type')
     this.obsPokemon.subscribe(this.faiqualcosa)
   }
 
-  faiqualcosa = (data: any) => {
+  faiqualcosa = (data: Risultati[]) => {
     this.data = data;
     console.log(data);
   }
